105f4bed915c87a1cbc757797f4aae880e30a7ca
shivam arora <shivamarora@live.com>
2018-04-15 14:49:10 +0530

    cors changed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
 /* Global Cross-Origin Access */
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Credentials", "*");
     res.header("Access-Control-Allow-Methods", "*");
     next();
