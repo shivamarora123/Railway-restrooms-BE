e9da38587b4aff46518d2ba9143a832d5a01e2fd
shivam arora <shivamarora@live.com>
2018-04-16 12:48:02 +0530

    morgon

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({
 }))
 
 
-app.use(morgan('combined')))
+app.use(morgan('combined'))
 /* Global Cross-Origin Access */
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
